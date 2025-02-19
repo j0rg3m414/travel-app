56042f34f0d0b2ad0d2585827e4613da84d93c5f
Jorge Maia <91641055391@tjce.jus.br>
2025-02-19 20:52:31 -0300

    Arquivos do projeto travel-app da formação confraria dev

---
+++ src/main/java/org/acme/travelorder/TravelOrderResource.java
@@ -0,0 +1,46 @@
+package org.acme.travelorder;
+
+import java.util.List;
+
+import org.hibernate.TransactionException;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("travelorder")
+public class TravelOrderResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TravelOrder> orders(){
+        return TravelOrder.listAll();
+    }
+
+    @GET
+    @Path("findById") 
+    public TravelOrder findById(@QueryParam("id") long id){
+        return TravelOrder.findById(id);
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public TravelOrder newTravelOrder(TravelOrder order){
+        try{
+            order.id = null;
+            order.persist();
+
+            return order;
+        }catch(TransactionException te){
+            te.printStackTrace();
+        }
+        return null;
+    }
+}
